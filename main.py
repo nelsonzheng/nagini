00c03bc6551976988075acbbcc0bd9da633d7e32
Daniel Buckmaster <dan.buckmaster@gmail.com>
2014-04-26 12:07:53 +1000

    Nearly there.

--- main.py
+++ main.py
@@ -6,37 +6,46 @@ import re
 
 @asyncio.coroutine
 def get_page(url):
-	response = yield from aiohttp.request('GET', url)
-	body = yield from response.read_and_close(decode = True)
-	return body
+    response = yield from aiohttp.request('GET', url)
+    body = yield from response.read_and_close(decode = True)
+    return body
 
 @asyncio.coroutine
 def get_areas(url):
-	page = yield from get_page(url)
-	soup = BeautifulSoup(page)
-	links = soup.find_all(
-		name = "a",
-		href = re.compile("^[A-Z]+\.html$")
-	)
-	return list(set([l['href'] for l in links]))
+    page = yield from get_page(url)
+    soup = BeautifulSoup(page)
+    links = soup.find_all(
+        name = "a",
+        href = re.compile("^[A-Z]+\.html$")
+    )
+    return list(set([l['href'] for l in links]))
 
 @asyncio.coroutine
 def get_subjects(url):
-	print(url)
-	return
-	page = yield from get_page(url)
-	soup = BeautifulSoup(page)
+    page = yield from get_page(url)
+    soup = BeautifulSoup(page)
+    links = soup.find_all(
+        name = "a",
+        href = re.compile("^[A-Z]{4}[0-9]{4}\.html$")
+    )
+    return list(set([l['href'] for l in links]))
+
+def flatten(l):
+    return [y for x in l for y in x]
 
 def scrape():
-	domain = 'http://www.timetable.unsw.edu.au/current/'
+    domain = 'http://www.timetable.unsw.edu.au/current/'
+    loop = asyncio.get_event_loop()
 
-	scraper = get_areas(domain + 'subjectSearch.html')
-	loop = asyncio.get_event_loop()
-	areas = loop.run_until_complete(scraper)
+    scraper = get_areas(domain + 'subjectSearch.html')
+    areas = loop.run_until_complete(scraper)
 
-	scraper = asyncio.wait([get_subjects(domain + area) for area in areas])
-	loop = asyncio.get_event_loop()
-	subjects = loop.run_until_complete(scraper)
+    scraper = asyncio.wait([get_subjects(domain + area) for area in areas])
+    done, _ = loop.run_until_complete(scraper)
+    subjects = flatten(list(map(lambda f: f.result(), done)))
+    print(len(subjects))
+
+    loop.close()
 
 if __name__ == "__main__":
-	scrape()
+    scrape()
